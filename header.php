d5932150a8d1362b274aef6031e5fcfe9f11a552
davidbernstein <hello@davidbernstein.ca>
2018-03-02 14:47:35 -0700

    add gitignore Navbar finished

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <body>
 <header class="sticky-top full-width">
     <div class=" nav-container">
-        <nav class="navbar primary-nav navbar-toggleable-md navbar-light">
+        <nav class="navbar  navbar-toggleable-md navbar-light">
 
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
