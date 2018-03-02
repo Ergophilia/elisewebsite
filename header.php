2257446f3fb3f17df5fefe58f5cb817565e3b8ba
davidbernstein <hello@davidbernstein.ca>
2018-03-02 11:29:20 -0700

    first commit

---
+++ header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Elise Static</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
+          integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <!--    Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<header class="sticky-top full-width">
+    <div class=" nav-container">
+        <nav class="navbar primary-nav navbar-toggleable-md navbar-light">
+
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a href=""><img src="http://eliseseehagel.web.dmitcapstone.ca/seehagel/wp-content/uploads/2018/02/logo.png"
+                            alt="logo"></a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item ">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">fitness</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Dance</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Wedding</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Make-up</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+</header>
+	
+
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php include 'header.php'; ?>
+<a href="http://eliseseehagel.web.dmitcapstone.ca/seehagel/fitness-2/">
+    <div class="frontpage-container">
+        <img src="http://eliseseehagel.web.dmitcapstone.ca/seehagel/wp-content/uploads/2018/02/fitness.png" alt="fitness" class="image">
+
+        <div class="maintext">Fitness</div>
+
+        <div class="overlay">
+            <div class="maintext">Fitness</div>
+        </div>
+    </div>
+</a>
+
+
+
+
+<!--  ------WEDDING------- -->
+<a href="http://eliseseehagel.web.dmitcapstone.ca/seehagel/wedding-2/">
+    <div class="frontpage-container">
+        <img src="http://eliseseehagel.web.dmitcapstone.ca/seehagel/wp-content/uploads/2018/02/wedding.png" alt="wedding" class="image">
+
+        <div class="maintext">Wedding</div>
+
+        <div class="overlay">
+            <div class="maintext">Wedding</div>
+        </div>
+    </div>
+</a>
+
+
+
+
+<!--     ------MAKEUP------- -->
+<a href="http://eliseseehagel.web.dmitcapstone.ca/seehagel/makeup-2/">
+    <div class="frontpage-container">
+        <img src="http://eliseseehagel.web.dmitcapstone.ca/seehagel/wp-content/uploads/2018/02/makeup.png" alt="makeup" class="image">
+
+        <div class="maintext">Makeup</div>
+
+        <div class="overlay">
+            <div class="maintext">Makeup</div>
+        </div>
+    </div>
+</a>
+
+<?php include 'footer.php'?>
+
